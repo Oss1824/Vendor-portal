1a308bf1cd1ca06ece8ab0f8373d883c481a2c68
Sudhanshu Yadav <sudhanshu.yadav@vultech.in>
2024-10-09 12:44:13 +0530

    added some services and reporters type

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { NotFoundPageComponent } from './component/not-found-page/not-found-page.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundPageComponent },
+];
